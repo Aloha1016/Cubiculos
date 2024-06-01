4feff8a9e1322c342c8c9d2acaa67221174ea7ff
jcrc770 <jc.ramirezcuellar@ugto.mx>
2024-05-31 18:38:43 -0600

    Solucion de diseño del login

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,37 +1,51 @@
 <template>
-  <div class="container-card">
-    <div class="container-form">
-      <div class="container-logo">
-        <img src="@/assets/logo.png" class="img-fluid mb-4" alt="ug image" />
+
+  <div class="container-xl my-4 py-4 rounded shadow" style="background-color: #002F6C;">
+    <div class="row d-flex justify-content-center">
+      <div class="col-md-6">
+        <div class="card shadow p-3 my-3">
+          <div class="d-flex justify-content-center pt-3">
+            <img src="@/assets/logo3.png" class="img-fluid t-imagen" alt="ug image" />
+          </div>
+          
+          <div class="px-5 py-1">
+            <form @submit.prevent="LogIn">
+                <div class="row">
+                  <div class="col-md-12 mb-3 d-flex">
+                    <i class="bi bi-person-fill fs-5 d-block w-25 py-2 rounded me-2 text-white" style="background-color: #002F6C;"></i>
+                    <input id="correo" type="email" v-model="email" placeholder="Email ID" class="form-control">
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-md-12 mb-3 d-flex">
+                      <i class="bi bi-lock-fill fs-5 d-block w-25 py-2 rounded me-2 text-white" style="background-color: #002F6C;"></i>
+                      <input id="contraseña" type="password" v-model="pass" placeholder="Password" class="form-control form-control">
+                  </div>
+                </div>
+                
+                <div class="mb-3">
+                  <input type="submit" value="Acceder" class="btn text-white w-100 mt-3" style="background-color: #002F6C;">
+                </div>
+                
+                <p class="text-secondary my-4">Al continuar, aceptas las <span>Condiciones de servicio</span>; y reconoces que leíste nuestra Política de privacidad.
+                Aviso de recopilación de información.</p>
+                
+              
+                <div class="row mt-3">
+                  <div class="col-12 col-md-6">
+                    <a href="/Register">¿Aún no estás en CubiBiblio?  Regístrate </a>
+                  </div>
+                  <div class="col-12 col-md-6">
+                    <a href="/Recuperacion">¿Olvidaste tu contraseña?  Recupérala aquí</a>
+                  </div>
+                </div>
+               
+            </form>
+          </div>
+        </div>
       </div>
-      <form @submit.prevent="LogIn">
-        <div class="campos">
-          <div class="campo">
-            <label for="correo">Correo </label>
-            <input type="email" v-model="email" placeholder="Correo Electrónico" class="formu">
-          </div>
-          <div class="campo">
-            <label for="contraseña">Contraseña</label>
-            <input type="password" v-model="pass" class="formu">
-          </div>
-        </div>
-        <div class="container-btn">
-          <input type="submit" value="Acceder" class="btn-aceptar">
-        </div>
-        <div class="opciones">
-          <p>Al continuar, aceptas las <span>Condiciones de servicio</span>; y reconoces que leíste nuestra</p>
-          <p><span>Política de privacidad.</span></p>
-          <p><span>Aviso de recopilación de información.</span></p>
-          <div class="container-registro">
-            <a href="/Register" class="registrate"><span class="s-registro">¿Aún no estás en CubiBiblio?</span><span>Regístrate </span></a>
-          </div>
-          <div class="container-recupera">
-            <a href="/Recuperacion" class="registrate"><span class="s-recupera">¿Olvidaste tu contraseña?</span><span>Recupérala aquí</span></a>
-          </div>
-        </div>
-      </form>
     </div>
-  </div>
+  </div> 
 </template>
 
 <script setup>
@@ -94,145 +108,10 @@ const LogIn = async () => {
 }
 </script>
 
+
 <style scoped>
-html {
-  box-sizing: border-box;
-  font-size: 62.5%; /* 1rem = 10px */
-  font-family: "personal", 'Courier New', Courier, monospace;
-}
-
-*, *:before, *:after {
-  box-sizing: inherit;
-}
-
-* {
-  padding: 0;
-  margin: 0;
-}
-
-img {
-  width: 100%;
-  height: 100%;
-}
-
-body {
-  background-image: url("@/assets/fondofooter.jpg");
-  background-repeat: no-repeat;
-  background-size: cover;
-  width: 100vw;
-  height: 100vh;
-}
-
-#app {
-  display: grid;
-  grid-template-columns: 1fr;
-}
-
-.container-card {
-  width: 90%;
-  margin: 0 auto;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 4rem;
-}
-
-.container-form {
-  font-size: 2.5rem;
-  border: .3rem solid #D4B75E;
-  width: 50%;
-  height: 65%;
-  padding: 4rem;
-  border-radius: 3rem;
-  color: #fff;
-  background-color: #EDEDEF;
-  box-shadow: .5rem .5rem 0 0 #D4B75E;
-}
-
-.container-logo {
-  width: 25rem;
-  text-align: center;
-  margin: 0 auto;
-  margin-bottom: 2rem;
-}
-
-.container-btn {
-  padding: 1rem;
-  display: flex;
-  justify-content: space-evenly;
-  margin-bottom: 2rem;
-}
-
-.container-btn .btn-aceptar {
-  width: 45%;
-  height: 4rem;
-  border-radius: .4rem;
-  border: none;
-  background-color: #D4B75E;
-  color: #ffffff;
-  font-size: 1.7rem;
-  line-height: 4rem;
-  font-weight: bold;
-  display: block;
-  cursor: pointer;
-}
-
-.opciones {
-  padding: 1.5rem;
-  font-size: 1.5rem;
-  text-align: center;
-  border-top: .1rem solid gray;
-  line-height: 2.2rem;
-  color: #295182;
-}
-
-span {
-  font-weight: bold;
-}
-
-.container-registro {
-  margin-top: 1.5rem;
-}
-
-.registrate {
-  color: #D4B75E;
-  text-decoration: none;
-}
-
-.s-registro {
-  font-weight: normal;
-  color: #295182;
-}
-
-.s-recupera {
-  color: #295182;
-  font-weight: normal;
-}
-
-.campo {
-  display: flex;
-  justify-content: center;
-  margin-bottom: 1.5rem;
-}
-
-label {
-  padding: 1rem;
-  background-color: #D4B75E;
-  border-radius: 1.4rem 0 0 1.4rem;
-  font-size: 1.4rem;
-  display: block;
-  width: 13rem;
-  text-align: center;
-  font-weight: bold;
-}
-
-.formu {
-  font-size: 1.4rem;
-  border-radius: 0 1.4rem 1.4rem 0;
-  padding: 1rem;
-  display: block;
-  text-align: center;
-  border-color: #D4B75E;
-  border: none;
+.t-imagen{
+  width: 30%;
+  margin-bottom:30px;
 }
 </style>
